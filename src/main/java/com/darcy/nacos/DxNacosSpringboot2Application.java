f5b04fe4bad81f7c069803d35a53c193855d10da
zhengxinjun <zheng.xinjun@darcytech.com>
2020-03-17 18:42:16 +0800

    init project

---
+++ src/main/java/com/darcy/nacos/DxNacosSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.darcy.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxNacosSpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxNacosSpringboot2Application.class, args);
+    }
+
+}
